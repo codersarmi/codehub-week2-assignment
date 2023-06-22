68d03b3c4033c3968f657dcc00416b2c1a315840
codersarmi <kaung.woody@gmail.com>
2023-06-22 21:30:57 +0630

    codehub-week2-assignment

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,72 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [id, setId] = useState("");
+  const [error, setError] = useState(false);
+  const [table, setTable] = useState(true);
   const [todo, setTodo] = useState([]);
 
-  const fetchData = async () => {
-    const respnse = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+  const getData = async (e) => {
+    e.preventDefault();
+    if (id < 1) {
+      setId("");
+      setError(true);
+      setTable(true);
+      return;
+    }
+    const respnse = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
     const data = await respnse.json();
     setTodo(data);
+    setTable(false);
+    setError(false);
+    setId("");
   };
 
   return (
     <section className="App">
-      <button onClick={fetchData}>Get Data</button>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {todo.map((e) => (
-            <tr key={e.id}>
-              <td>{e.id}</td>
-              <td>{e.title}</td>
-              <td>
-                {e.completed ? (
-                  <p className="completed">Completed</p>
-                ) : (
-                  <p className="incompleted">Incompleted</p>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <form onSubmit={getData}>
+        <input
+          type="number"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <button type="submit">Get Data</button>
+        <div>
+          {error && <p>Please enter a valid number (eg. 1 , 2 , 3 , ....)</p>}
+        </div>
+        <div>
+          {!table && (
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>UserID</th>
+                    <th>Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{todo.id}</td>
+                    <td>{todo.title}</td>
+                    <td>{todo.userId}</td>
+                    <td>
+                      {todo.completed ? (
+                        <p className="completed">Completed</p>
+                      ) : (
+                        <p className="incompleted">Incompleted</p>
+                      )}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </form>
     </section>
   );
 }
